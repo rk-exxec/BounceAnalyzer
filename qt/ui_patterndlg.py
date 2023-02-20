416c052a3aaeaef6cea7578e5c4d28c65b8d046a
Raphael <40037381+rk-exxec@users.noreply.github.com>
2023-02-20 17:35:53 +0100

    add batch process dialog

---
+++ qt/ui_patterndlg.py
@@ -0,0 +1,78 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'patterndlg.ui'
+##
+## Created by: Qt User Interface Compiler version 6.4.2
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
+    QMetaObject, QObject, QPoint, QRect,
+    QSize, QTime, QUrl, Qt)
+from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
+    QFont, QFontDatabase, QGradient, QIcon,
+    QImage, QKeySequence, QLinearGradient, QPainter,
+    QPalette, QPixmap, QRadialGradient, QTransform)
+from PySide6.QtWidgets import (QAbstractButton, QApplication, QDialogButtonBox, QHBoxLayout,
+    QLabel, QLineEdit, QPushButton, QSizePolicy,
+    QVBoxLayout, QWidget)
+
+class Ui_PatternDialog(object):
+    def setupUi(self, PatternDialog):
+        if not PatternDialog.objectName():
+            PatternDialog.setObjectName(u"PatternDialog")
+        PatternDialog.resize(353, 136)
+        self.verticalLayout = QVBoxLayout(PatternDialog)
+        self.verticalLayout.setObjectName(u"verticalLayout")
+        self.label = QLabel(PatternDialog)
+        self.label.setObjectName(u"label")
+
+        self.verticalLayout.addWidget(self.label)
+
+        self.horizontalLayout = QHBoxLayout()
+        self.horizontalLayout.setObjectName(u"horizontalLayout")
+        self.openBtn = QPushButton(PatternDialog)
+        self.openBtn.setObjectName(u"openBtn")
+
+        self.horizontalLayout.addWidget(self.openBtn)
+
+        self.rootPathTxt = QLineEdit(PatternDialog)
+        self.rootPathTxt.setObjectName(u"rootPathTxt")
+
+        self.horizontalLayout.addWidget(self.rootPathTxt)
+
+
+        self.verticalLayout.addLayout(self.horizontalLayout)
+
+        self.label_2 = QLabel(PatternDialog)
+        self.label_2.setObjectName(u"label_2")
+
+        self.verticalLayout.addWidget(self.label_2)
+
+        self.patternTxt = QLineEdit(PatternDialog)
+        self.patternTxt.setObjectName(u"patternTxt")
+
+        self.verticalLayout.addWidget(self.patternTxt)
+
+        self.buttonBox = QDialogButtonBox(PatternDialog)
+        self.buttonBox.setObjectName(u"buttonBox")
+        self.buttonBox.setStandardButtons(QDialogButtonBox.Cancel|QDialogButtonBox.Ok)
+
+        self.verticalLayout.addWidget(self.buttonBox)
+
+
+        self.retranslateUi(PatternDialog)
+
+        QMetaObject.connectSlotsByName(PatternDialog)
+    # setupUi
+
+    def retranslateUi(self, PatternDialog):
+        PatternDialog.setWindowTitle(QCoreApplication.translate("PatternDialog", u"PatternDialog", None))
+        self.label.setText(QCoreApplication.translate("PatternDialog", u"Root directory", None))
+        self.openBtn.setText(QCoreApplication.translate("PatternDialog", u"Open...", None))
+        self.label_2.setText(QCoreApplication.translate("PatternDialog", u"Pattern", None))
+        self.patternTxt.setText(QCoreApplication.translate("PatternDialog", u"*.cihx", None))
+    # retranslateUi
+
